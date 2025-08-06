b59b317aede8f9a2c4171a3d3c8bd9f4e48c1c7c
prashantkumar999 <prashant047alg@gmail.com>
2025-08-06 21:36:17 +0530

    basic functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useDebugValue, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [input, setInput] = useState("")
+  const [apiData, setApiData] = useState([]);
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const fetchData = async () => {
+        const data = await fetch("https://dummyjson.com/recipes/search?q="+input);
+        const json = await data.json()
+        setApiData(json.recipes)
+      }
+      if (input.trim() !== "") {
+        fetchData();
+      } else {
+        setApiData([])
+      }
+    }, 500)
+    return () => clearTimeout(timer);
+
+  }, [input])
+  console.log(apiData)
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='main-container'>
+        <div className='input-container'>
+          <input className='input-tag' type='text' placeholder='search here' value={input} onChange={(e) => setInput(e.target.value)} />
+        </div>
+        {
+          input && <div className='results-div'>
+          {
+            apiData.map((data) => <div className='recommendation' key={data.id}>
+              {data.name}
+            </div>)
+          }
+        </div>
+        }
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
